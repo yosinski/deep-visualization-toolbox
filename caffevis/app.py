6ed844f13db84b9a2c06e9b1d7769de656407ca4
Jason Yosinski <jason@yosinski.com>
2015-08-12 17:00:40 +0100

    fixed minor bug

--- caffevis/app.py
+++ caffevis/app.py
@@ -46,8 +46,8 @@ class CaffeVisApp(BaseApp):
         excess_h = self._data_mean.shape[1] - self.settings.caffevis_data_hw[0]
         excess_w = self._data_mean.shape[2] - self.settings.caffevis_data_hw[1]
         assert excess_h >= 0 and excess_w >= 0, 'mean should be at least as large as %s' % repr(self.settings.caffevis_data_hw)
-        self._data_mean = self._data_mean[:, excess_h:(excess_h+self.settings.caffevis_data_hw[0]),
-                                          excess_w:(excess_w+self.settings.caffevis_data_hw[1])]
+        self._data_mean = self._data_mean[:, (excess_h/2):(excess_h/2+self.settings.caffevis_data_hw[0]),
+                                          (excess_w/2):(excess_w/2+self.settings.caffevis_data_hw[1])]
         self._net_channel_swap = (2,1,0)
         self._net_channel_swap_inv = tuple([self._net_channel_swap.index(ii) for ii in range(len(self._net_channel_swap))])
         self._range_scale = 1.0      # not needed; image comes in [0,255]
